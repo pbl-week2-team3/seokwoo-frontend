1c480065ced61d8d081f414f42241560f3c6b35e
jsw4215 <jsw4215van@gmail.com>
2022-02-20 02:12:10 +0900

    infinity scroll

--- src/shared/firebase.js
+++ src/shared/firebase.js
@@ -1,13 +1,23 @@
 import firebase from "firebase/app";
 import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
 
 const firebaseConfig = {
-  // 인증정보!
+  apiKey: "AIzaSyDKID6d19YTj_TKMsygwoaxKCpi8TktyOU",
+  authDomain: "reactweek2-1161e.firebaseapp.com",
+  projectId: "reactweek2-1161e",
+  storageBucket: "reactweek2-1161e.appspot.com",
+  messagingSenderId: "87127436567",
+  appId: "1:87127436567:web:c78fde668001d85eb71b52",
+  measurementId: "G-TES4LMJ7TC"
 };
 
 firebase.initializeApp(firebaseConfig);
 
 const apiKey = firebaseConfig.apiKey;
 const auth = firebase.auth();
+const firestore = firebase.firestore();
+const storage = firebase.storage();
 
-export{auth, apiKey};
+export{auth, apiKey, firestore, storage};
